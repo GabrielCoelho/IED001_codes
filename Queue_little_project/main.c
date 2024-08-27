030c36dc2cb3aa40871c763332ce2ba6068f213f
GabrielCoelho <gbcoe@outlook.com.br>
2024-08-27 19:42:31 -0300

    feat(Queue): Init of a little project in class

---
+++ Queue_little_project/main.c
@@ -0,0 +1,10 @@
+// Copyright «2024» Gabriel Coelho Soares
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  int queue_array[5];
+
+  return EXIT_SUCCESS;
+}
---
+++ Queue_little_project/queuefn.h
@@ -0,0 +1,7 @@
+// Copyright «2024» Gabriel Coelho Soares
+#ifndef QUEUEFN_H
+#define QUEUEFN_H
+
+enum { TAMANHO = 5 };
+
+#endif  // !QUEUEFN_H
