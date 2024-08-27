ab5b72cf9ea4050df30e14bc6b01a875b147e02a
GabrielCoelho <gbcoe@outlook.com.br>
2024-08-27 20:47:40 -0300

    fix(push & move): Fixed the problems with pushing and moving a value from the queue.

--- Queue_little_project/main.c
+++ Queue_little_project/main.c
@@ -7,23 +7,30 @@
 
 int main(int argc, char *argv[]) {
   int vetor_exemplo[TAMANHO] = {5, 2, 3, 9, 4};
-
-  for (int i = 0; i < TAMANHO; i++) {
-    printf("Inserindo o valor %d na fila\n", vetor_exemplo[i]);
-    if (!push_queue(vetor_exemplo[i])) {
-      printf("Fila cheia");
-    }
-  }
-  show_vec();
-  printf("\n\n");
   int retirado = 0;
-  for (int i = 0; i < TAMANHO; i++) {
-    printf("Retirando o valor ");
-    if (move_queue(&retirado)) {
-      printf("%d da fila\n", retirado);
-    } else {
-      printf("ERRO DE FILA!!!\n");
-    }
-  }
+  // Mostra pilha vazia
+  show_vec();
+  // Início da inserção
+  if (!push_queue(5)) printf("Fila cheia\n");
+  if (!push_queue(2)) printf("Fila cheia\n");
+  if (!push_queue(3)) printf("Fila cheia\n");
+  if (!push_queue(9)) printf("Fila cheia\n");
+  // Mostra os valores atuais
+  show_vec();
+  if (!push_queue(4)) printf("Fila cheia\n");
+  if (!push_queue(15)) printf("Fila cheia\n");  // vai mostrar fila cheia
+  // mostra os valores da fila
+  show_vec();
+
+  // Início da remoção
+  if (!move_queue(&retirado)) printf("ERRO DE FILA!!!\n");
+  if (!move_queue(&retirado)) printf("ERRO DE FILA!!!\n");
+  if (!move_queue(&retirado)) printf("ERRO DE FILA!!!\n");
+  show_vec();
+  if (!move_queue(&retirado)) printf("ERRO DE FILA!!!\n");
+  if (!move_queue(&retirado)) printf("ERRO DE FILA!!!\n");
+  if (!move_queue(&retirado))
+    printf("ERRO DE FILA!!!\n");  // mostra Erro de Fila
+  show_vec();
   return EXIT_SUCCESS;
 }
--- Queue_little_project/queuefn.c
+++ Queue_little_project/queuefn.c
@@ -9,9 +9,10 @@ int queue_end = INICIO_FILA;
 int queue_start = INICIO_FILA;
 
 bool push_queue(int value) {
-  if (queue_end == FIM_FILA && queue_start == FIM_FILA) {
+  if (queue_end == FIM_FILA || queue_start == FIM_FILA) {
     return false;  // Fila cheia
   }
+  printf("Inserindo o valor %d na fila\n", value);
   vec_queue[queue_end] = value;
   queue_end++;
   return true;
@@ -21,6 +22,7 @@ bool move_queue(int* value) {
   if (queue_start == queue_end || queue_start == FIM_FILA) {
     return false;
   } else {
+    printf("Retirando o valor %d da fila\n", vec_queue[queue_start]);
     *value = vec_queue[queue_start];
     queue_start++;
     return true;
@@ -28,7 +30,12 @@ bool move_queue(int* value) {
 }
 
 void show_vec() {
-  for (int i = 0; i < FIM_FILA; i++) {
-    printf("Fila[%d] = %d\n", i + 1, vec_queue[i]);
+  if (!(queue_start == queue_end)) {
+    for (int i = queue_start; i < queue_end; i++) {
+      printf("Fila[%d] = %d\n", i + 1, vec_queue[i]);
+    }
+  } else {
+    printf("Fila vazia");
   }
+  printf("\n\n");
 }
