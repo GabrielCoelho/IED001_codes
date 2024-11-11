1fe26e4e091fae2a02e436515543f0f7830f16ad
GabrielCoelho <gbcoe@outlook.com.br>
2024-11-11 09:23:39 -0300

    fix(btree.h): Refactored at Header file

--- Assignment_Final/btree.h
+++ Assignment_Final/btree.h
@@ -21,7 +21,7 @@ extern struct PKey *btree_current;
 
 void initialize_btree();
 void finalize_btree(struct PKey *who);
-bool localize_btree(char cpf_searched, struct PKey *where);
+struct PKey *localize_btree(char cpf_searched, struct PKey *where);
 struct PKey *newData_btree(char cpf);
 void addNewData_btree(struct PKey *who, struct PKey *where);
 void removeData_btree(char cpf);
